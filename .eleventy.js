d87d9e70706bbe7f1f6bff3a48f77a8483ed51d3
shreya-naarla <naarlash@msu.edu>
2025-03-16 22:17:51 +0000

    commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
-
 // The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("images");  
   eleventyConfig.addPassthroughCopy("styles");
 };
+
+
+
+
--- about.md
+++ about.md
@@ -3,5 +3,3 @@ layout: layout.liquid
 title: About
 ---
 
-# About the 11ty **Boilerplate**
-<img class="about" alt="pine" src="/images/pine.png" width="50" />
